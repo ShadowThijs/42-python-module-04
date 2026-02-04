e7763658dc53dc5bf6b839ff36c645ca366ceeba
tlogtenb <logtenbergthijs@yahoo.com>
2026-02-04 16:46:55 +0100

    ex0 done

---
+++ ex0/ft_ancient_text.py
@@ -0,0 +1,22 @@
+"""Text recovery from a file."""
+
+
+file_name: str = "ancient_fragment.txt"
+
+print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===\n")
+print(f"Accessing Storage Vault: {file_name}")
+
+try:
+    # open(file_name, "r")
+    with open(file_name, "r") as file:
+        data: str = file.read()
+        print("Connection established!\n")
+        print("Recovered data from storage vault: ")
+        print(data)
+        print()
+        print("Data recovery complete. Storage unit disconnected.")
+        file.close()
+except FileNotFoundError:
+    print(f"Connection failed: {file_name} not found")
+except PermissionError:
+    print(f"Connection failed: {file_name} needs elevated priveleges")
